adda0be8e82f4e43d1f7a2c8ffc4025189424c95
yadavp473131 <yadavp8120@gmail.com>
2024-04-26 17:22:31 +0530

    first commit

---
+++ frontend/src/api.js
@@ -0,0 +1,25 @@
+import axios from "axios"
+import { ACCESS_TOKEN } from "./constants"
+
+// const apiUrl = "/choreo-apis/awbo/backend/rest-api-be2/v1.0";
+
+const api = axios.create({
+    // baseURL: import.meta.env.VITE_API_URL
+    baseURL: import.meta.env.VITE_API_URL 
+})
+
+// check wheather we have our access token in localstorage
+api.interceptors.request.use(
+    (config) =>{
+      const token = localStorage.getItem(ACCESS_TOKEN);
+      if(token){
+        config.headers.Authorization = `Bearer ${token}`
+      }
+      return config
+    },
+    (error)=>{
+        return Promise.reject(error)
+    }
+)
+
+export default api
